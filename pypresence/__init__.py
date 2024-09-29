6949be09aacb563129444400e76245fd5c7aee19
Physics2022 <brandoyeets@gmail.com>
2024-09-28 20:37:52 -0400

    init

---
+++ pypresence/__init__.py
@@ -0,0 +1,10 @@
+from .presence import Presence
+
+# class Presence:
+#     def __init__(self, client_id, pipe=0):
+#         pass
+#     def connect():
+#         pass
+    
+#     def update(self, **kwargs):
+#         pass
---
+++ pypresence/presence.py
@@ -0,0 +1,9 @@
+class Presence:
+    def __init__(self,  *args, **kwargs):
+        pass
+
+    def connect(self):
+        pass
+    
+    def update(self, *args, **kwargs):
+        pass
